9d1c855fbccfbf771982e75ef037becb4f6522a2
byronyz <14061883+byronyz@user.noreply.gitee.com>
2025-12-16 11:32:47 +0800

    init 版

--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
 magick \
-  \( "0010.jpg" -resize 2480x1754\> -background white -gravity center -extent 2480x1754 \) \
-  \( "0011.jpg" -resize 2480x1754\> -background white -gravity center -extent 2480x1754 \) \
-  +append \
-  -units PixelsPerInch -density 300x300 \
+  \( "0011.jpg" -resize "1754x1240>" -background white -gravity center -extent 1754x1240 \) \
+  \( "0012.jpg" -resize "1754x1240>" -background white -gravity center -extent 1754x1240 \) \
+  -append \
+  -page "1754x2480" \
+  -units PixelsPerInch \
+  -density 300 \
   "merged_output.pdf"
+
+open "merged_output.pdf"
