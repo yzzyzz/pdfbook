af42f4170315ee7f6a3aa61fae68e54622962987
byronyz <14061883+byronyz@user.noreply.gitee.com>
2026-01-04 20:20:53 +0800

    fixbug

--- dankai2a42x2.py
+++ dankai2a42x2.py
@@ -384,14 +384,14 @@ def draw_images_in_a5_region(canvas_obj, image_files, a5_index, x_offset,
         # 每个A5区域1张图片
         if pdf_page_index % 2 == 0:  # 正面
             if a5_index % 2 == 1:  # 右边
-                img_index = need_A4_pages * 4 - int(pdf_page_index) - 1
+                img_index = int(pdf_page_index)
             else:
-                img_index = int(pdf_page_index) 
+                img_index = need_A4_pages * 4 - int(pdf_page_index) - 1
         else:  # 反面
             if a5_index % 2 == 1:  # 右边
-                img_index = int(pdf_page_index) 
-            else:
                 img_index = need_A4_pages * 4 - int(pdf_page_index) - 1
+            else:
+                img_index = int(pdf_page_index)
         img_path = image_files[img_index] if img_index < len(
             image_files) else None
         page_number = img_index + 1
@@ -448,7 +448,7 @@ def draw_images_in_a5_region(canvas_obj, image_files, a5_index, x_offset,
                         page_x = x_offset + 4 + center_padding
                     else:
                         page_x = x_offset + a5_width - text_width - 4 - center_padding
-                page_y = y_offset + 3
+                page_y = y_offset + 8
                 canvas_obj.drawString(page_x, page_y, page_number_text)
 
     elif CURRENT_A5_IMAGE_COUNT == A5_IMAGES_2:
